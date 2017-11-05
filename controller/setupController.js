84e3c392a8b619dcea15dcae87733e9815f9c2dc
arausly <arausid@yahoo.com>
2017-11-05 19:56:55 +0100

    added ip-whiteListing and & auth

--- controller/setupController.js
+++ controller/setupController.js
@@ -10,12 +10,21 @@ module.exports= function(server,restify,restifyValidator){
     server.use(morgan('dev'));
 
     server.use(restify.authorizationParser());
-
-    server.use(function(req,res,next){
+    //  how to utilize re.authorization api 
+    //  call restify.authorizationParser()
+    //  write the middleware function
+    //  client :{
+    //  authorization: basic{
+    //    username:what the client inputs as username
+    //    passwords: what the client inputs as password 
+    // }
+    //  }
+    //  if it tallys with my written middleware function. then user is authenticated.
+    server.use(function (req,res,next){
          let apiKeys = {
-             "user1":'userapiKe278292sdbds7292dbjwebjwe',
+             "user1":'password',
          }
-         if(typeof(req.authorization.basic) === "undefined" || !apiKeys[req.authorization.basic.username] || req.authorization.basic.password !== apiKeys[req.authorization.basic.password]){
+         if(typeof(req.authorization.basic) === "undefined" || !apiKeys[req.authorization.basic.username] || req.authorization.basic.password !== apiKeys[req.authorization.basic.username]){
             let response = {
                 status:'failure',
                 data:'You must specify a valid api key',
@@ -24,8 +33,28 @@ module.exports= function(server,restify,restifyValidator){
             res.writeHead(403);
             res.end(JSON.stringify(response));
             return next();
-         }else{
-             return next();
          }
+           return next();
+    });
+    server.use(function(req,res,next){
+         let whiteListedIps = ['10.0.3.232','127.0.0.12','125.0.12.232'];
+         let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+          if(whiteListedIps.indexOf(ip) === -1){
+            let response = {
+                status:'failure',
+                data:'Invalid Ip address',
+            };
+            res.setHeader('content-type','application/json');
+            res.writeHead(403);
+            res.end(JSON.stringify(response));
+            return next();
+          }
+          return next();
     })
+    server.use(restify.throttle({
+        rate:1,
+        burst:2,
+        ip:true,
+        xff:true,
+    }))
 }
